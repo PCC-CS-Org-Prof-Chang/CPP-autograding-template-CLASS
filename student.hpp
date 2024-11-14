97710562443febd4b279a5ab8409cee800110cfe
Ken Chang - CS Professor <154487739+adjprof2@users.noreply.github.com>
2024-11-13 19:45:10 -0800

    delegate implementation to Calculator.cpp

--- student.hpp
+++ student.hpp
@@ -3,7 +3,7 @@
 #include <iostream> // print library
 #include "./AbstractDataType/calculator.h" // reference implementations
 
-// Exercise 1: implements function 'add', that adds two numbers in the Caclulator class
+// Exercise 1: implements member function 'add', that adds two numbers in the Caclulator class in calculator.cpp
 
-// Exercise 2: implements function 'mul', that multiplies two numbers in the Caclulator class
+// Exercise 2: implements member function 'mul', that multiplies two numbers in the Caclulator class in calculator.cpp
 
